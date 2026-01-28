e9094ecb43697d6029a840df7ec04d7c24dca325
achrafrf <itsachrafrafiq@gmail.com>
2026-01-29 00:09:54 +0100

    fix: add paths to tsconfig and fix import casing 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,23 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <Script
-            id="orchids-browser-logs"
-            src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts/orchids-browser-logs.js"
-            strategy="afterInteractive"
-            data-orchids-project-id="504b9c01-e771-4c77-838c-e978ddcf8e10"
-          />
           {/* <ErrorReporter /> */}
-          <Script
-            src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts//route-messenger.js"
-            strategy="afterInteractive"
-            data-target-origin="*"
-            data-message-type="ROUTE_CHANGE"
-            data-include-search-params="true"
-            data-only-in-iframe="true"
-            data-debug="true"
-            data-custom-data='{"appName": "YourApp", "version": "1.0.0", "greeting": "hi"}'
-          />
           {children}
           <VisualEditsMessenger />
         </ThemeProvider>
